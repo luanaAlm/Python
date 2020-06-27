218dae6c48dfdf806a5370d25caf8ee7ff01a0ca
Luana Rodrigues Almeida <52934698+luanaAlm@users.noreply.github.com>
2020-06-27 10:50:16 -0300

    Activity

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,11 @@
+"""
+Recebendo dados do usuário
+"""
+# Enrtrada de dados
+print('qual o seu nome?')
+nome = input()
+
+print('seja berm-vindo {0}' .format(nome))
+print('qual sua idade?')
+idade = input()
+print('tenho {0}' .format(idade))
