a9945f297717e9e133001ec0f95aebe3dd93d6b4
Luana Rodrigues Almeida <52934698+luanaAlm@users.noreply.github.com>
2020-07-09 14:55:03 -0300

    Create Loop_for.py

---
+++ Loop_for.py
@@ -0,0 +1,13 @@
+"""
+Loop for
+é uma estrutura de repetição
+"""
+#for 1 interando uma string
+nome ='Geek University'
+lista =[1, 3, 5, 7, 9]
+numeros = range (1,10)
+
+for letra in nome:
+    print(letra)
+
+# for 2 iterando sobre uma lista
