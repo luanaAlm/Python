f9d09110f61ac1db4a34fa1887f2b6ec03a1187a
Luana Rodrigues Almeida <52934698+luanaAlm@users.noreply.github.com>
2020-07-09 14:16:22 -0300

    Update Codicionais_if_else_elif.py
    Estruturas codicionais if, else, elif

--- Codicionais_if_else_elif.py
+++ Codicionais_if_else_elif.py
@@ -1,3 +1,12 @@
 """
-
+Estruturas codicionais
+if (se), else, elif
 """
+idade = 18
+
+if idade < 18:
+    print('Menor de idade')
+elif idade == 18:
+    print('tem 18 anos')
+else:
+    print('Maior de idade')
