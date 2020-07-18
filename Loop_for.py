303587dd01ac98eff1f06c5615387b2f67c42521
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-07-18 10:46:56 -0300

    Update Loop_for.py

--- Loop_for.py
+++ Loop_for.py
@@ -11,3 +11,9 @@ for letra in nome:
     print(letra)
 
 # for 2 iterando sobre uma lista
+for numero in lista:
+    print(numero)
+
+#for 3 interando sobre o renge
+for numero in range(1, 10):
+    print(numero)
