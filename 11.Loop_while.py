7372a053123cd1820415eff650edd83e45374422
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-07-20 12:16:20 -0300

    Update 11.Loop_while.py

--- 11.Loop_while.py
+++ 11.Loop_while.py
@@ -1,7 +1,14 @@
 """
 Loop While
-"""
+
+print('Tipo 1')
 num = 1
-while num < 5:
-    print(num)
+while num < 10:
+    print('feliz dia do amigo')
     num = num + 1
+"""
+print('------')
+print('Tipo 2')
+resposta = ''
+while resposta != 'sim':
+        resposta = input('Feliz dia do amigo')
