8d4f5d01e11fde550728aefcde11cbab42e4620b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-07-20 16:52:55 -0300

    Create 11.Loop_while.py

---
+++ Estruturas_de_repeticao/11.Loop_while.py
@@ -0,0 +1,14 @@
+"""
+Loop While
+
+print('Tipo 1')
+num = 1
+while num < 10:
+    print('feliz dia do amigo')
+    num = num + 1
+"""
+print('------')
+print('Tipo 2')
+resposta = ''
+while resposta != 'sim':
+        resposta = input('Feliz dia do amigo')
