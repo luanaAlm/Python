60d00785b6f713801d8fdcf5a9becfe6d5bdc2cf
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-07-20 09:54:29 -0300

    Update Loop_for.py

--- Loop_for.py
+++ Loop_for.py
@@ -7,6 +7,7 @@ nome ='Geek University'
 lista =[1, 3, 5, 7, 9]
 numeros = range (1,10)
 
+"""
 for letra in nome:
     print(letra)
 
@@ -17,3 +18,23 @@ for numero in lista:
 #for 3 interando sobre o renge
 for numero in range(1, 10):
     print(numero)
+    
+#enumerate numera cada letra pondo assim um indice a cada letra
+for indice, letra in enumerate(nome):
+    print(letra)
+
+for valor in enumerate(nome):
+    print(valor)
+
+print("Soma")
+qtd = int(input("Quantas vezes o loop deve rodar?"))
+soma = 0
+for n in range(1, qtd+1):
+    num = int(input(f'informe o {n}/{qtd} valor: '))
+    soma = soma + num
+    print(f'A soma e: {soma}')
+
+"""
+print("imprimir todas as letras em uma so linha")
+for letra in nome:
+    pr int(letra, end='')
