6f5d5550f87af1a2e372baf3075d59f61e4b7fad
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-07-20 16:45:39 -0300

    Update 01.Listas.py

--- Colecoes_Python/01.Listas.py
+++ Colecoes_Python/01.Listas.py
@@ -71,7 +71,7 @@ lista2.reverse()
 print('com reverse',lista2)
 print('com [::-1]',lista1[::-1])
 """
-
+"""
 print('Copiar uma lista')
 lista7 = lista2.copy()
 print(lista7)
@@ -102,6 +102,84 @@ print('converter lista em string')
 texto = ['programação', 'em', 'Python:', 'Essencial']
 print(texto)
 
-print('join, pegar string e tranforma em texto')
+print('join, pegar string e tranforma em texto string')
 curso = ' '.join(texto)
-print('1',curso)
+print(curso)
+"""
+"""
+print('Interando sobre listas')
+for elemento in lista2:
+    print(elemento)
+
+print('-----')
+print('Utilizando While')
+
+carrinho = []
+produto = ''
+while produto != 'sair':
+    print("Adicione mais um ou digite sair")
+    produto = input()
+    if produto != 'sair':
+        carrinho.append(produto)
+for produto in carrinho:
+    print(produto)
+"""
+"""
+print('listas com variaveis')
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+
+numeros = [num1, num2, num3, num4, num5]
+print(numeros)
+"""
+"""
+cores = ['verde', 'amarelo', 'azul', 'branco']
+print(cores[0])
+print(cores[1])
+print(cores[2])
+print(cores[3])
+print(cores[-2])
+print(cores[-3])
+print(cores[-4])
+
+for indice, cor in enumerate(cores):
+    print(indice,cor)
+"""
+"""
+print('lista com slice (indice)')
+
+lista = [1, 2, 3, 4]
+print(lista[1:])
+print(lista[1:])
+print(lista[1:3])
+
+print('-----')
+print('soma',sum(lista))
+print('maximo',max(lista))
+print('minimo',min(lista))
+print('tamanho',len(lista))
+"""
+
+print('copiando lista para outra (shallow copy e Deep copy')
+print('forma 1 - DEEP COPY')
+lis = [1, 2, 3]
+print(lis)
+nova = lis.copy()
+print(nova)
+nova.append(4)
+print(lis)
+print(nova)
+print('-----')
+print('forma 1 - SHALLOW COPY')
+
+lis = [1, 2, 3]
+print(lis)
+nova = lis
+nova.append(4)
+print(lis)#lista com a adição
+print(nova)#vira uma nova lista com a adição
