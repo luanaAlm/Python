1c39f3d42a22e571970d56beefaf919e541469cf
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-07-21 11:42:13 -0300

    Create 03.Dicionarios.py

---
+++ Colecoes_Python/03.Dicionarios.py
@@ -0,0 +1,12 @@
+"""
+Dicionarios tambem conhecidos como mapas
+São coleçoes do tipo chave/valor
+Dicionarios são representados por chaves
+
+print(type({}))
+"""
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+print(type(paises))
+print(paises)
+
+
