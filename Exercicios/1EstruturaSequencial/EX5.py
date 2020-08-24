4eca41ee19dcbe9ae84347c80117cd3d136f20cd
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-08-24 11:39:46 -0300

    Create EX5.py

---
+++ Exercicios/1EstruturaSequencial/EX5.py
@@ -0,0 +1,6 @@
+"""Faça um Programa que converta metros para centímetros."""
+conv = 0
+metros = (float(input('Digite os metros: ')))
+conv = metros * 100
+print('metros: {} '.format(metros))
+print('é igual a centrimetros: {} '.format(conv))
