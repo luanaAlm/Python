8ca39d6cf69547c0da5f3d69a04f4da6954a51e2
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-08-24 11:40:07 -0300

    Create EX8.py

---
+++ Exercicios/1EstruturaSequencial/EX8.py
@@ -0,0 +1,11 @@
+"""
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+Calcule e mostre o total do seu salário no referido mês.
+"""
+soma = 0
+valor = float(input('Quanto você ganha por hora? '))
+hora = int(input('Número de horas trabalhadas no mês? '))
+soma = valor * hora
+print('Ganho: {}'.format(soma))
+
+"https://wiki.python.org.br/EstruturaSequencial"
