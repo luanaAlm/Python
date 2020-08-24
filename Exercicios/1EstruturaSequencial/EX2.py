e4886a447adcacc911ff1a434c45973e389b3bd8
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-08-24 11:39:33 -0300

    Create EX2.py

---
+++ Exercicios/1EstruturaSequencial/EX2.py
@@ -0,0 +1,4 @@
+"""Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]."""
+print('digite um numero')
+numero = input()
+print('O número informado foi', numero)
