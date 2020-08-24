b35f5538783873cae6f9238ac0a185a195028ed9
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-08-24 11:39:54 -0300

    Create EX6.py

---
+++ Exercicios/1EstruturaSequencial/EX6.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
+area = 0
+raio = float(input('digite o raio de umm circulo: '))
+area = 3.14 * (raio * raio)
+print('A area do circulo é: {}'.format(area))
