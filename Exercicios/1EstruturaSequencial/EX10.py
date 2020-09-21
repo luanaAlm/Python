3336b213aa7c8900b9d82c959856bd37f76720e7
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-09-21 10:01:19 -0300

    Create EX10.py

---
+++ Exercicios/1EstruturaSequencial/EX10.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+"""
+Celsius = float(input('Digite o valor dde graus em Celsius: '))
+Fahrenheit = ((Celsius * 9)/5) + 32
+print('O valor em Fahrenheit é: {}'.format(Fahrenheit))
