90d4899f3733fcfb68722e47b6d057f2b9bd8326
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-09-21 15:21:23 -0300

    Update 02.funcoes_com_retorno.py

---
+++ 5.Funcoes/02.funcoes_com_retorno.py
@@ -0,0 +1,22 @@
+"""
+Funções com retorno
+
+numeros = [1, 2, 3, 4, 5]
+#pop retorna o ultimo numero da lista
+ret_pop = numeros.pop()
+print(f'retorno de pop:{ret_pop}')
+#print nao retorna a nada
+ret_pr = print(numeros)
+print(f'O retorno de print: {ret_pr}')
+"""
+"""
+funções que retornam valores, devem retornar esses valores com a palavra reservada return
+
+def quadrado_de_7():
+    return 7*7
+ret = quadrado_de_7()
+print(f'forma1\n'
+      f'Retorno: {ret}')
+print(f'\nforma2\n'
+      f'Retorno: {quadrado_de_7()}')
+"""
