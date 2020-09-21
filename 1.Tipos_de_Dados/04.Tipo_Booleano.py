f0591a47a3d61704068da328b59006f9a1072bda
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-09-21 14:41:42 -0300

    Create 04.Tipo_Booleano.py

---
+++ 1.Tipos_de_Dados/04.Tipo_Booleano.py
@@ -0,0 +1,31 @@
+"""
+Tipo booleano
+"""
+ativo = True
+logado= False
+print(ativo)
+
+"""
+Operações basicas
+"""
+#negacao (not)
+"""
+Se o valor for verdadeiro o not negara
+se o vslor for falso o not afirmara
+"""
+print('Valor de Not')
+print(not ativo)
+
+#Ou (or)
+"""
+Depende de dois valores, um ou outro deve ser verdadeiro
+"""
+print('or')
+print(ativo or logado)
+
+#e (end)
+"""
+Depende de dois valores, os dois valores devem ser verdadeiros
+"""
+print('end')
+#print(ativo end logado)
