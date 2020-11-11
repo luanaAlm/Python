dccdc386114e1bf7a38a07c607f3dd42078f5146
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-11 09:28:17 -0300

    Create digite.py

---
+++ Exercicios/1EstruturaSequencial/digite.py
@@ -0,0 +1,6 @@
+input('Qual o seu nome')
+input('Cidade que nasceu')
+anoNasc = int(input('Digite o ano que nasceu'))
+anos = anoNasc - 2020
+
+print('Sua idade é : {}'.format(anos) + 'idade a 10 anos é : {}'.format())
