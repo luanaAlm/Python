258ad0f36705e9fe6ab28cf427da0e385a15c40b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-23 11:25:50 -0300

    Create EX1.py

---
+++ Exercicios/2EstruturaDeDecisao/EX1.py
@@ -0,0 +1,11 @@
+"""
+Faça um Programa que peça dois números e imprima o maior deles.
+"""
+num1 = int(input("Digite o 1 numero: "))
+num2 = int(input("Digite o 2 numero: "))
+
+print('O maior número é:')
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
