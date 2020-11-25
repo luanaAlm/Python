df0f2506f31aca4e9265561defaacbf4b1369f6d
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-25 18:09:20 -0300

    Update EX3.py

--- Exercicios/2EstruturaDeDecisao/EX3.py
+++ Exercicios/2EstruturaDeDecisao/EX3.py
@@ -2,5 +2,11 @@
 Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 """
-letra = input('Digite f: feminino e m: masculino')
+letra = input('Digite f: feminino e m: masculino\n')
+if letra == 'm':
+    print('Masculino')
+if letra == 'f':
+    print('Feminino')
+else:
+    print('Letra invalida')
 
