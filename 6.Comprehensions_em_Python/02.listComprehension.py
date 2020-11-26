e95c780c79b3b5d96d51ecf30f286b3e7cc89e28
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-26 09:25:50 -0300

    Create 02.listComprehension.py

---
+++ 6.Comprehensions_em_Python/02.listComprehension.py
@@ -0,0 +1,16 @@
+"""
+List Comprehension
+Adicionando estruturas condicionais lógicas
+"""
+#exemplo 1
+numeros = [1, 2, 3, 4, 5, 6]
+pares = [numero for numero in numeros if numero % 2 == 0]
+impares = [numero for numero in numeros if numero % 2 != 0]
+print(pares)
+print(impares)
+print('----')
+#Negação
+pares = [numero for numero in numeros if not numero % 2]
+impares = [numero for numero in numeros if numero % 2]
+print(pares)
+print(impares)
