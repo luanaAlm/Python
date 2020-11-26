5f5d5b8791e1c58d127fe29d2f8aad5958ae98c6
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-26 10:34:41 -0300

    Create 05.setComprehension.py

---
+++ 6.Comprehensions_em_Python/05.setComprehension.py
@@ -0,0 +1,11 @@
+"""
+ Set Comprehension
+"""
+numeros = {num for num in range(1, 7)}
+print(numeros)
+
+numeros = {x ** 2 for x in range(10)}
+print(numeros)
+
+letras = {letra for letra in 'Geek University'}
+print(letras)
