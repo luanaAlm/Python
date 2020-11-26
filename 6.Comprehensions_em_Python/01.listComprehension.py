1df8f2d97542cfd6f36906d56240ec9ee630c384
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-11-26 09:25:46 -0300

    Update 01.listComprehension.py

--- 6.Comprehensions_em_Python/01.listComprehension.py
+++ 6.Comprehensions_em_Python/01.listComprehension.py
@@ -2,7 +2,7 @@
 List Comprehension
 pode-se gerar novas listas com dados prcessados a partir de outro interável
 """
-
+"""
 #sintaxe do List Comprehension
 numeros = [1, 2, 3, 4, 5]
 res = [numero * 10 for numero in numeros]
@@ -13,3 +13,13 @@ def funcao(valor):
 
 res = [funcao(numero) for numero in numeros]
 print(res)
+print([numero * 2 for numero in [1, 2, 3, 4, 5]])
+"""
+nome = 'Geek University'
+print([letra.upper() for letra in nome])
+
+def caixa_alta(nome):
+    nome = nome.replace(nome[0], nome[0].upper())
+    return nome
+amigos = ['maria', 'julia', 'carlos', 'andrá', 'marcos']
+print([caixa_alta(amigo) for amigo in amigos])
