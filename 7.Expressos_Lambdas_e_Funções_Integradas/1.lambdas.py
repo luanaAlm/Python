64d671e9f7823a1e04db14836f03971422fcdd23
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-07 16:17:55 -0300

    Update 1.lambdas.py

--- 7.Expressos_Lambdas_e_Funções_Integradas/1.lambdas.py
+++ 7.Expressos_Lambdas_e_Funções_Integradas/1.lambdas.py
@@ -1,6 +1,6 @@
 """
 São funçõoes sem nome ou seja função anônimas
-"""
+
 
 def funcao(x):
     return 3 * x + 1
@@ -17,3 +17,21 @@ print(calc(4))
 nome_completo = lambda nome, sobrenome: nome.strip().title() + ' ' + sobrenome.strip().title()
 print(nome_completo('maria', 'sousa'))
 print(nome_completo('FERNANDA     ', 'soares'))
+
+#em funções python podemos ter nenhuma ou varis entradas. em lambda tmb
+amar = lambda : 'Como não amar python'
+uma = lambda x: 3 * x + 1
+duas = lambda x, y: (x * 5) ** 0.5
+tres = lambda x, y, z: 3 / (1/ x + 1 / y + 1 / z)
+#n = lambda x1, x2, ..., xn: <expressão>
+print(amar())
+print(uma(6))
+print(duas(5, 7))
+print(tres(3, 6, 9))
+"""
+#Outros exxemplos
+nomes = ['Isaac Asivon', 'Marcos Vinicios', 'Gorete V. Lacerda', 'Lazaro Sousa', 'Maria O. C. Vilani',
+           'Julioc Cesar', 'H. G. Cruz', 'Reinaldo Maraes', 'Daniel Marques', 'Heitor Enzo']
+print(nomes)
+nomes.sort(key=lambda sobrenome: sobrenome.split(' ')[-1].lower())
+print(nomes)
