56bcedd2bc09e93caafcb70dc5f6caf77df2c4de
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-09 15:36:57 -0300

    Create 10.Len_Abs_Sum_Round.py

---
+++ 7.Expressos_Lambdas_e_Funções_Integradas/10.Len_Abs_Sum_Round.py
@@ -0,0 +1,22 @@
+"""
+Len(): retorna o tamanho (o numero de itens de um interavel
+"""
+print('len:', len('Sacola de lixo'))
+print('len:', len([1, 2, 3, 4, 5]))
+
+"""
+Abs(): Retorna o vslor absoluto de um numero  inteiro ou real 
+"""
+print(abs(-5)) #retorna positivo
+
+"""
+Sum(): soma de interáveis
+"""
+print(sum([1, 2, 3, 4, 5]))
+print(sum((2,5)))
+
+"""
+Round(): Retorna um numero arredondado
+"""
+print(round(10.50))
+print(round(10.501))
