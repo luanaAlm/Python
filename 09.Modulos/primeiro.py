e5dca317a894b314f8716d833a5c17aec4892e65
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-14 15:00:43 -0300

    Create primeiro.py

---
+++ 09.Modulos/primeiro.py
@@ -0,0 +1,8 @@
+def funcao1():
+    print('Teste Dunder')
+
+if __name__ == '__main__':
+    funcao1()
+    print('primeiro.py esta sendo execultado diretamente')
+else:
+    print('primeiro.py foi importado')
