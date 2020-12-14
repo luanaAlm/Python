c834cceb9a50ff8a572c527d240c10a9cafd4de9
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-14 15:00:33 -0300

    Create 05.Dunder_Main_e_Dunder_Name.py

---
+++ 09.Modulos/05.Dunder_Main_e_Dunder_Name.py
@@ -0,0 +1,11 @@
+"""
+Dunder: dublee under
+Dunder Main: __main__
+Dunder Name: __name__
+
+São utilizadas para criar funções, atributos, propriedades, para não gerar conflitos
+com o nome desses elementos na programação
+ """
+import primeiro
+import segundo
+import colorama
