74f6fa883feff86f72845f82cf8d636abe976b76
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-14 11:03:59 -0300

    .

--- 09.Modulos/01.modulo_random.py
+++ 09.Modulos/01.modulo_random.py
@@ -11,10 +11,33 @@ import random
 #primeiro random -> pacote
 # segundo random() -> função
 print(random.random())
-"""
 
 #Forma 2 - Importando uma função especifica do random
 from random import random
 
 for i in range(10):
     print(random())
+print('\n')
+#uniform() -> Gera um número pseudo-aleatório entre os valores estabelecidos
+from random import uniform
+for i in range(10):
+    print(uniform(3, 7)) #O 7 não será incluido
+print('\n')
+#randint() -> Gera um número pseudo-aleatório entre os valores estabelecidos
+from random import randint
+for i in range(6):
+    print(randint(1, 61), end='. ')
+print('\n')
+#choice() -> Mostra um valor aleatório entre interáveis
+from random import choice
+jogador = ['pedra', 'papel', 'tesoura']
+print(choice(jogador))    
+"""
+print('\n')
+#shuffle() -> Embaralha números interáveis
+from random import shuffle
+cartas = ['K', 'Q', 'J', 'A', '2', '3', '4', '5', '6', '7']
+print(cartas)
+shuffle(cartas)
+print(cartas)
+print('Carta:',cartas[0])
