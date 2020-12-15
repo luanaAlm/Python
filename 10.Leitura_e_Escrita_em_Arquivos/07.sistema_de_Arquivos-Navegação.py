d5fe92bd122d44a719ce7f61164c66506f897bb4
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-15 11:41:11 -0300

    Create 07.sistema_de_Arquivos-Navegação.py

---
+++ 10.Leitura_e_Escrita_em_Arquivos/07.sistema_de_Arquivos-Navegação.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Sistema de Arquivos - Navegação
+Para fazer manipulação de arquivos no SO, imprta-se e faz-se uso do módulo os
+"""
+import os
+
+"""
+#getcwd() -> pega o current work directory (diretório de trabalho corrente)
+print(os.getcwd())
+#chdir -> Muda o diretorio
+os.chdir('..')
+print(os.getcwd())
+
+os.chdir('..')
+print(os.getcwd())
+
+os.chdir('..')
+print(os.getcwd())
+
+os.chdir('..')
+print(os.getcwd())
+
+os.chdir('..')
+print(os.getcwd())
+
+os.chdir('..')
+print(os.getcwd())
+
+#Saber se diretório é absoluto ou relativo
+print(os.path.isabs('C:\\user\\'))
+
+#Identificar o SO com o módulo os
+print(os.name)
+
+#Detalhes do sistema peracional
+print(os.uname())
+"""
