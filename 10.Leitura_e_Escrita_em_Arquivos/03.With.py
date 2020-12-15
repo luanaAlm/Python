9faf3501ac611405ad6a53cc0c964da37259152f
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-15 10:51:42 -0300

    Update 03.With.py

--- 10.Leitura_e_Escrita_em_Arquivos/03.With.py
+++ 10.Leitura_e_Escrita_em_Arquivos/03.With.py
@@ -1,3 +1,10 @@
 """
-With
+With: é utilizado para criar um contexto de trabalho onde os recursos utilizadas são fechados
+após o bloco with
 """
+#forma profissional de manipular arquivos
+with open('teste.txt') as arquivo:
+    print(arquivo.readlines())
+    print(arquivo.closed)
+
+print(arquivo.closed)
