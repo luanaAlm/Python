c8ee79157388995eb62eba07e40f1c42e3719b85
Luana Rodirgues <luanarodrigues3211@gmail.com>
2020-12-22 15:07:54 -0300

    Create 02.classes.py

---
+++ 13.Orientação_a_Objetos_com_Python/02.classes.py
@@ -0,0 +1,14 @@
+"""
+POO - Classes
+    São modelos dos objetos do mundo real sendo representado computacionalmente
+    Classes podem conter:
+        - Atributos
+        - Métodos (funções)
+"""
+#Ex Sisitema para automação de lampadas
+
+class Lampada:
+    pass
+
+lamp = Lampada()
+print(type(lamp))
