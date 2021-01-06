88b2b52fa56fcda720abfa61f9959f2840f56932
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-01-06 15:10:43 -0300

    Create 05.herancaMultipla.py

---
+++ 14.Herança_e_Polimorfismo/05.herancaMultipla.py
@@ -0,0 +1,5 @@
+"""
+POO - Herança Múltipla
+é a possibilidade de uma classe herdar de múltiplas classes, fazendo com que a classe filha herde todos
+os atributos e metodos de todas as classes herdadas
+"""
